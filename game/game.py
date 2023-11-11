82d5a482a91d97a9d2795567f96f67e371f4d2d8
10604kdy <n.01062234594@gmail.com>
2023-11-11 11:59:36 +0900

    충돌 검사하기

--- game/game.py
+++ game/game.py
@@ -2,11 +2,17 @@ from constants.image import BACKGROUND_IMAGE
 from game.player import Player
 from game.enemy import Enemy
 import random
+import math
 class Game:
     def __init__(self, screen):
         self.screen = screen
         self.player = Player()
         self.enemies=[ Enemy()]
+
+    def __is_collided(self,rect1,rect2,boundary_distance):
+        distance=math.sqrt((rect1.center[0]-rect2.center[0])**2 + (rect1.center[1]-rect2.center[1]) ** 2)
+        return distance < boundary_distance
+
     def update(self):
         self.player.update()
 
@@ -15,6 +21,14 @@ class Game:
 
         if random.randint(1,100)<3:
             self.enemies.append(Enemy())
+
+        new_enemies=[]
+
+        for enemy in self.enemies:
+            if self.__is_collided(self.player.rect,enemy.rect,15):
+                continue
+            new_enemies.append(enemy)
+        self.enemies=new_enemies
 
 
 
